6888e3cb760b70ac4eb521b1532bd62a7adc0a0f
tonmoysaha <tonmoysahaopi@gmail.com>
2020-08-26 01:28:48 -0700

    adding todos list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './component/login/login.component';
 import {FormsModule} from '@angular/forms';
 import { WelcomeComponent } from './component/welcome/welcome.component';
 import { ErrorComponent } from './component/error/error.component';
+import { ListTodosComponent } from './component/list-todos/list-todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     WelcomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    ListTodosComponent
   ],
   imports: [
     BrowserModule,
