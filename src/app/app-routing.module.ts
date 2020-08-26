58f68ed39d55778116426b180a0ec3984a4992e8
tonmoysaha <tonmoysahaopi@gmail.com>
2020-08-26 01:42:33 -0700

    updater todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {WelcomeComponent} from './component/welcome/welcome.component';
 import {LoginComponent} from './component/login/login.component';
 import {ErrorComponent} from './component/error/error.component';
+import {ListTodosComponent} from './component/list-todos/list-todos.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'welcome/:name', component: WelcomeComponent},
+  {path: 'todos' , component: ListTodosComponent},
   {path: '' , component: LoginComponent},
   {path: '**', component: ErrorComponent}
 ];
--- src/app/component/list-todos/list-todos.component.ts
+++ src/app/component/list-todos/list-todos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-list-todos',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
-  constructor() { }
+  todos = [
+    {id: 1, description: 'Walk need to done'},
+    {id: 2, description: 'Walk need to done'},
+    {id: 3, description: 'Walk need to done'},
+    {id: 4, description: 'Walk need to done'}
+    ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
