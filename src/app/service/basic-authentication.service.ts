487f29ad8145fc9efe92969ae04d1877c9e00bd9
tonmoysaha <tonmoysahaopi@gmail.com>
2020-11-06 04:12:37 -0800

    basic auth service

---
+++ src/app/service/basic-authentication.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HelloWorldBean} from './data/welcomedata.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  executeBasicAuthenticationService(username, password) {
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    let headers = new HttpHeaders({
+      Authorization : basicAuthHeaderString
+    });
+    let url = `http://localhost:8080/basicAuth`;
+    return this.httpClient.get<string>(url , {headers});
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
+
+  }
+
+  logout(){
+    sessionStorage.removeItem('authenticatedUser');
+  }
+
+
+}
