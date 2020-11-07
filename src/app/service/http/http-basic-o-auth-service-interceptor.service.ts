deeafa9f812ce047267089483b56bd5edda9f825
tonmoysaha <tonmoysahaopi@gmail.com>
2020-11-07 01:37:33 -0800

    update interceptor

--- src/app/service/http/http-basic-o-auth-service-interceptor.service.ts
+++ src/app/service/http/http-basic-o-auth-service-interceptor.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {BasicAuthenticationService} from '../basic-authentication.service';
+import {JwtAuthenticationServiceService} from '../jwt-authentication-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpBasicOAuthServiceInterceptorService implements HttpInterceptor{
 
-  constructor(private basicAuthService: BasicAuthenticationService) { }
+  constructor(private jwtAuthService: JwtAuthenticationServiceService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    let basicAuthHeaderString = this.basicAuthService.getAuthenticationToken();
-    let userName = this.basicAuthService.getAuthencatedUser();
+    let jwtAuthHeaderString = this.jwtAuthService.getAuthenticationToken();
+    let userName = this.jwtAuthService.getAuthencatedUser();
 
-    if (basicAuthHeaderString && userName) {
+    if (jwtAuthHeaderString && userName) {
       request = request.clone({
         setHeaders: {
-          Authorization: basicAuthHeaderString
+          Authorization: jwtAuthHeaderString
         }
       });
     }
